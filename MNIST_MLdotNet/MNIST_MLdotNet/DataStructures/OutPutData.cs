00b03e11606b2d75519731e78fa32b3a37104c43
gianpaolo.santopaolo@ibv.ch <gianpaolo.santopaolo@ibv,ch>
2019-11-12 10:31:00 +0100

    initial VS solutions commit

---
+++ MNIST_MLdotNet/MNIST_MLdotNet/DataStructures/OutPutData.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MNIST_MLdotNet.DataStructures
+{
+    class OutPutData
+    {
+        [ColumnName("Score")]
+        public float[] Score;
+    }
+}
